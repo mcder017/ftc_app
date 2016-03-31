ae2e8c40c665519590f82bda7441158bb04ca3a5
HardComma222 <pcmesloh@gmail.com>
2016-03-31 16:21:33 -0400

    Test Commit, Can be deleted later

---
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/testClassforFTC.java
@@ -0,0 +1,21 @@
+package ftcRobot;
+
+import java.util.Scanner;
+
+public class testClassforFTC {
+	public static Scanner portFolioScanner = new Scanner(System.in);
+	// declare variables here
+	static boolean intVariable;
+    static String nameVariable;
+	static boolean letterVariable;
+	static int intletterVariable;
+	static int intVowel;
+	static boolean didIGuessWord = false;
+	static int wordLength;
+	static boolean stopAsk = false;
+	static boolean getallVowels;
+	static int numberLocation;
+	static boolean stopAsk2 = false;
+	static int intConsenant;
+	static boolean stopAskC;
+}
